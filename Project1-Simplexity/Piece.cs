1dd37bf05f6a4226e83a9dd670e412259f89416e
Francisco Freixo <freixosurf@hotmail.com>
2018-04-20 23:12:38 +0000

    Create Visualisation

---
+++ Project1-Simplexity/Piece.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_Simplexity
+{
+    enum Piece
+    {
+        None, RedCube, WhiteCube, RedCilinder, WhiteCilinder
+    }
+}
--- Project1-Simplexity/Renderer.cs
+++ Project1-Simplexity/Renderer.cs
@@ -8,5 +8,50 @@ namespace Project1_Simplexity
 {
     class Renderer
     {
+        public void Render(Board board)
+        {
+
+            string[,] gamesymbols = new string[7, 7];
+
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Coordinates coordinates = new Coordinates(i, j);
+                    gamesymbols[i, j] = DefineGameSymbol(board.GetPiece(coordinates));
+
+                    Console.Write(gamesymbols[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public string DefineGameSymbol(Piece piece)
+        {
+            string temp = "";
+
+            switch (piece)
+            {
+                case Piece.RedCube:
+                    temp = "R";
+                    break;
+                case Piece.WhiteCube:
+                    temp = "W";
+                    break;
+                case Piece.RedCilinder:
+                    temp = "r";
+                    break;
+                case Piece.WhiteCilinder:
+                    temp = "w";
+                    break;
+                default:
+                    temp = "|";
+                    break;
+
+            }
+            return temp;
+
+        }
     }
 }
