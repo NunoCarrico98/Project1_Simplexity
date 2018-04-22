f14178000d5df3bd0b8b04bc6d3d9ea3f7ac2c11
Francisco Freixo <freixosurf@hotmail.com>
2018-04-22 15:36:08 +0100

    Add comments to Shape, Color and Piece Enums

--- Project1-Simplexity/Color.cs
+++ Project1-Simplexity/Color.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Project1_Simplexity
 {
+    /// <summary>
+    /// Enumeration that defines Color Variants.
+    /// </summary>
     enum Color 
     {
         None , Red, White
